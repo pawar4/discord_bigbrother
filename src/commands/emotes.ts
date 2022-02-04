5a8ae319ce1d1e675a3e59b3e5722593f9cd2508
pawar4 <pawar4@purdue.edu>
2022-02-04 16:52:48 -0500

    fixed bugs with added emotes functionality

--- src/commands/emotes.ts
+++ src/commands/emotes.ts
@@ -8,6 +8,7 @@ import {
     SlashOption
 } from "discordx";
 import fs from "fs";
+import emotes from "./../assets/emotes.json";
 
 @Discord()
 @SlashGroup("emotes", "We're too poor for Nitro")
@@ -17,19 +18,21 @@ export abstract class Emotes {
         @SlashOption("name") name: string, 
         command: CommandInteraction,
     ) {
-        let emotes = JSON.parse("./src/assets/emotes.json");
-        if (fs.existsSync("./src/assets/emotes/" + name + ".gif"))
+        
+        // let emotes = JSON.parse(fs.readFileSync("./src/assets/emotes.json"));
+        if (fs.existsSync("./src/assets/emotes/" + <string>name + ".gif"))
         {
             command.reply({files: ["./src/assets/emotes/" + name + ".gif"]});
         }
         else if (emotes.gifs.hasOwnProperty(name)) {
-            command.reply(emotes.gifs[name] + ".gif");
+            let test = <keyof typeof emotes.gifs>name;
+            command.reply(emotes.gifs[test] + ".gif");
         }
         else{
             command.reply(
-                "I'm probably ruining the moment here but Idk that emote chief. \
-                I made you look dumb but like my creater likes to say: it be like \
-                that sometimes"
+                "I'm probably ruining the moment here but Idk that emote chief. "
+                + "I made you look dumb but like my creater likes to say, it be like "
+                + "that sometimes."
             );
         }
         
