63d16e35b2f42f2a11f511a2fe5eef2ddfb41719
pawar4 <pawar4@purdue.edu>
2022-02-04 15:36:08 -0500

    added support for more emotes with links

--- src/commands/emotes.ts
+++ src/commands/emotes.ts
@@ -10,19 +10,27 @@ import {
 import fs from "fs";
 
 @Discord()
-@SlashGroup("emotes", "We're too poor for Nitro")
+// @SlashGroup("emotes", "We're too poor for Nitro")
 export abstract class Emotes {
-    @Slash("send")
+    @Slash("emote")
     sendEmote(
         name: string, 
         command: CommandInteraction,
     ) {
+        let emotes = JSON.parse("./src/assets/emotes.json");
         if (fs.existsSync("./src/assets/emotes/" + name + ".gif"))
         {
             command.reply({files: ["./src/assets/emotes/" + name + ".gif"]});
         }
+        else if (emotes.gifs.hasOwnProperty(name)) {
+            command.reply(emotes.gifs[name] + ".gif");
+        }
         else{
-            command.reply("I dont know that meme");
+            command.reply(
+                "I'm probably ruining the moment here but Idk that emote chief. \
+                I made you look dumb but like my creater likes to say: it be like \
+                that sometimes"
+            );
         }
         
     }
