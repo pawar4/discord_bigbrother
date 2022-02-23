7bc93610562a5136ce3c2d3a8b08c8590423dc95
pawar4 <pawar4@purdue.edu>
2022-02-22 20:16:16 -0500

    added emotes change. Not fully tested

--- src/commands/emotes.ts
+++ src/commands/emotes.ts
@@ -1,4 +1,4 @@
-import { AutocompleteInteraction, CommandInteraction } from "discord.js";
+import { CommandInteraction } from "discord.js";
 import {
     Discord,
     SimpleCommand,
@@ -8,7 +8,19 @@ import {
     SlashOption,
     SlashChoice,
 } from "discordx";
-import emotes from "./../assets/emotes.json";
+import fs from "fs";
+import * as schedule from "node-schedule";
+
+let jsonfile = fs.readFileSync('./src/assets/emotes.json', "utf8");
+let emotes = JSON.parse(jsonfile);
+
+let rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 18;
+rule.minute = 0;
+schedule.scheduleJob(rule, async function () {
+    fs.writeFileSync('./src/assets/emotes.json', JSON.stringify(emotes));
+})
 
 @Discord()
 // @SlashGroup("emotes", "We're too poor for Nitro")
@@ -16,10 +28,20 @@ export abstract class Emotes {
     @Slash("emote")
     sendEmote(
         @SlashChoice(emotes.gifs)
-        @SlashOption("name",)
-        name: string,
+        @SlashOption("name") name: string,
         interaction: CommandInteraction,
     ) {
         interaction.reply(name + ".gif");        
+    }
+
+    @Slash("add_emote")
+    addEmote(
+        @SlashOption("name",) name: string,
+        @SlashOption("url") url: string,
+        interaction: CommandInteraction,
+    ) {
+        emotes.gifs[name] = url;
+        console.log(emotes);
+        interaction.reply("Done :)");
     }
 }
